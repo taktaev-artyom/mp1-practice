f3d5cb7e96ff3ad8df53e4804ecef3e78d22df83
taktaev-artyom <artaktaev@gmail.com>
2018-12-23 22:44:37 +0300

    Release Practice5

--- taktaev_aa/Practice/Practice5/main.c
+++ taktaev_aa/Practice/Practice5/main.c
@@ -1,182 +1,393 @@
+#include <windows.h>
 #include <stdio.h>
-#include <locale.h>
-#define k 10
-#define N 5
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+#define MAX_LEN 5000
+#define MAX_FILES 2000
+int kolvo = 0;
 
-void choose(int a[], int n)
+void choose(ULONGLONG* filesize, int kolvo, int* newindex)
 {
-    int i, j, min, minidx, tmp;
-    for(i = 0; i < n; i++)
+    int i, j, minind;
+    ULONGLONG min;
+    for (i = 0; i < kolvo; i++)
     {
-        min = a[i];
-        minidx = i;
-        for(j = i + 1; j < n; j++)
+        min = filesize[i];
+        minind = i;
+        for (j = i + 1; j < kolvo; j++)
         {
-            if (a[j] < min)
+            if (filesize[j] < min)
             {
-                minidx = j;
-                min = a[j];
+                min = filesize[j];
+                minind = j;
+                newindex[i] = j;
+                filesize[minind] = filesize[i];
+                filesize[i] = min;
             }
         }
-        tmp = a[i];
-	    a[i] = a[minidx];
-        a[minidx] = tmp;
     }
 }
- 
-void insert(int a[], int n)
+
+void insert(ULONGLONG* filesize, int* newindex, int kolvo)
 {
-    int i, j, tmp;
-    for(i = 1; i < n; i++)
+    int i, j, swap;
+    ULONGLONG tmp;
+    for (i = 0; i < kolvo; i++)
     {
         j = i - 1;
-        tmp = a[i];
-        while ((j >= 0) && (a[j] > tmp))
+        tmp = filesize[i];
+        swap = newindex[i];
+        while ((j >= 0) && (filesize[j] > tmp))
         {
-            a[j + 1] = a[j];
-            a[j] = tmp;
+            filesize[j + 1] = filesize[j];
+            filesize[j] = tmp;
+            newindex[j + 1] = newindex[j];
+            newindex[j] = swap;
             j--;
         }
-    } 
+    }
 }
 
-void bubblesort(int a[], int n)
+void bubblesort(ULONGLONG* filesize, int* newindex, int kolvo)
 {
-    int i, j, tmp;
-    for (i = 0; i < n; i++)
+    int i, j, swap;
+    ULONGLONG tmp;
+    for (i = 0; i < kolvo; i++)
     {
-        for (j = 1; j < n - i; j++)
+        for (j = 1; j < kolvo; j++)
         {
-            if (a[j - 1] > a[j])
+            if (filesize[j - 1] > filesize[j])
             {
-                tmp = a[j];
-                a[j] = a[j - 1];
-                a[j - 1] = tmp;
+                tmp = filesize[j];
+                filesize[j] = filesize[j - 1];
+                filesize[j - 1] = tmp;
+                swap = newindex[j];
+                newindex[j] = newindex[j - 1];
+                newindex[j - 1] = swap;
             }
         }
     }
 }
 
-void countingsort(int a[], int n)
+void countingsort(ULONGLONG* a, int kolvo, int* newindex)
 {
     int i, j, b = 0;
-    int count[k];
-    for (i = 0; i < k; i++) count[i] = 0;
-    for (i = 0; i < n; i++) count[a[i]]++;
+    ULONGLONG* count;
+    ULONGLONG* dopmass;
+    ULONGLONG min = a[0], max = a[kolvo - 1], k;
+    dopmass = (ULONGLONG*)malloc(kolvo * sizeof(ULONGLONG));
+    for (i = 0; i < kolvo; i++)
+    {
+        dopmass[i] = a[i];
+    }
+    for (i = 0; i < kolvo; i++)
+    {
+        if (a[i] < min)
+            min = a[i];
+    }
+    for (i = 0; i < kolvo; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+    }
+    k = max + min + 1;
+    count = (ULONGLONG*)malloc(k * sizeof(ULONGLONG));
+    for (i = 0; i < k; i++)
+        count[i] = 0;
+    for (i = 0; i < kolvo; i++)
+    {
+        count[a[i] - min]++;
+    }
     for (i = 0; i < k; i++)
     {
         for (j = 0; j < count[i]; j++)
-        {
-            a[b] = i;
-            b++;
-        }
+            a[b++] = i + min;
+    }
+    for (j = 0; j < kolvo; j++)
+        b = 0;
+    for (i = 0; i < kolvo; i++)
+    {
+        for (j = 0; j < kolvo; j++)
+            if ((a[i] == dopmass[j]))
+            {
+                newindex[b] = j;
+                dopmass[j] = -1;
+                b++;
+                break;
+            }
     }
 }
 
-void quicksplit(int a[], int *i, int *j, int p)
+int* quicksort(ULONGLONG* size, int n1, int n2, int n, int* id)
 {
-    do 
-    {
-        while (a[*i] < p) i++;
-        while (a[*j] > p) j--;
-        if (*i < *j)
+    ULONGLONG tmp;
+    int i = n1, j = n2, p = size[(n1 + n2) / 2], tmp1;
+    do {
+        while (size[i] < p) i++;
+        while (size[j] > p) j--;
+        if (i <= j)
         {
-            int tmp = a[*i];
-            a[*i] = a[*j];
-            a[*j] = tmp;
+            if (size[i] > size[j])
+            {
+                tmp = size[i];
+                size[i] = size[j];
+                size[j] = tmp;
+                tmp1 = id[i];
+                id[i] = id[j];
+                id[j] = tmp1;
+            }
+            i++;
+            j--;
         }
-    } while (*i <= *j);
+    } while (i <= j);
+    if (i < n2)
+        id = quicksort(size, i, n2, n, id);
+    if (n1 < j)
+        id = quicksort(size, n1, j, n, id);
+    return id;
 }
 
-void quicksort(int a[], int n1, int n2)
+void merge(int* a, ULONGLONG* size, int l, int m, int r) 
 {
-    int pidx = (n1 + n2) / 2;
-    int i = n1, j = n2;
-    quicksplit(a, &i, &j, a[pidx]);
-    if (j > n1) quicksort(a, n1, j);
-    if (i < n2) quicksort(a, i, n2);
+    int i, j = m + 1, h, tmp;
+    ULONGLONG tmp1;
+    for (i = l; ((i < r) && (j <= r)); i++)
+    {
+        if (size[a[i]] > size[a[j]])
+        {
+            tmp = a[j];
+            for (h = j; h > i; h--)
+                a[h] = a[h - 1];
+            a[i] = tmp;
+            j++;
+        }
+    }
+
 }
 
-void mergesort(int a[], int l, int r)
+void mergesort(int* a, ULONGLONG* size, int l, int r)
 {
     int m;
-	if (l >= r) return;
+    if (l >= r) return;
     m = (l + r) / 2;
-    mergesort(a, l, m);
-    mergesort(a, m+1, r);
-    merge(a, l, m, r);
+    mergesort(a, size, l, m);
+    mergesort(a, size, m + 1, r);
+    merge(a, size, l, m, r);
 }
 
-void merge(int a[], int l, int m, int r)
-{
-    int i = 0, j = m + 1, s = 0; 
-    while ((i < m) && (j < r)) 
-        if (a[i] < a[j]) a[s++] = a[i++];
-        else a[s++] = a[j++];
-    while (i < m) a[s++] = a[i++];
-    while (j < r) a[s++] = a[j++];
-}
-
-void scan(int a[], int n)
+void zeroing(ULONGLONG* filesize, ULONGLONG* filesizecopy, int* filesindex, int* newindex, int kolvo)
 {
     int i;
-    printf("¬ведите массив:\n");
-    for (i = 0; i < n; i++) scanf("%d", &(a[i]));
-}
-
-void print(int a[], int n)
-{
-    int i;
-    printf("¬аш массив:\n");
-    for (i = 0; i < n - 1; i++) printf("%d", a[i]);
-    printf("%d\n", a[n - 1]);
-}
-
-int menu()
-{
-    int r;
-    printf("¬ыберите вид сортировки:\n");
-    printf("1.—ортировка выбором.\n");
-    printf("2.—ортировка вставками.\n");
-    printf("3.ѕузырькова¤ сортировка.\n");
-    printf("4.—ортировка подсчЄтом.\n");
-    printf("5.Ѕыстра¤ сортировка.\n");
-    printf("6.—ортировка сли¤нием.\n");
-    scanf("%d", &r);
-    return r;
-}
-
-int choose(int rezhim)
-{
-	switch (rezhim)
+    for (i = 0; i < kolvo; i++)
     {
-    case 1:
-        choose(a,N);
-        break;
-    case 2:
-        insert(a,N);
-        break;
-    case 3:
-        bubblesort(a,N);
-        break;
-    case 4:
-        countingsort(a,N);
-        break;
-    case 5:
-        quicksort(a, 0, N - 1);
-        break;
-    case 6:
-        mergesort(a, 0, N - 1);
-        break;
-    default: printf("ќшибка в выборе сортировки\n");
-             return;
+        filesize[i] = filesizecopy[i];
+        newindex[i] = filesindex[i];
+
+    }
 }
 
+int ListDirectoryContents(const wchar_t* sDir, ULONGLONG* filesize, wchar_t** fileNames)
+{
+    int i = 0;
+    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+    wchar_t *sPath;
+    sPath = (wchar_t*)malloc(MAX_FILES * sizeof(wchar_t));
+    wsprintf(sPath, L"%s\\*.*", sDir);
+    if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+    {
+        wprintf(L"Path not found: [%s]\n", sDir);
+        return 1;
+    }
+    do
+    {
+        if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+        {
+            ULONGLONG fileSize = fdFile.nFileSizeHigh;
+            fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
+            fileSize |= fdFile.nFileSizeLow;
+            filesize[i] = fileSize;
+            fileNames[i] = (wchar_t*)malloc(MAX_FILES * sizeof(wchar_t));
+            wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
+            wsprintf(fileNames[i], L"%s", sPath);
+            i++;
+            kolvo++;
+        }
+    } while (FindNextFile(hFind, &fdFile));
+    FindClose(hFind);
+    return 0;
+}
+
+void print(ULONGLONG* filesize, wchar_t** fileNames, int kolvo)
+{
+    int i;
+    for (i = 0; i < kolvo; i++)
+    {
+        wprintf(L"file  %s size %lld bytes\n", fileNames[i], filesize[i]);
+    }
+}
+void menu()
+{
+    printf("Choose the type of sorting:\n");
+    printf("1. Selection sort\n");
+    printf("2. Insertion sort\n");
+    printf("3. Bubble sort\n");
+    printf("4. Counting sort\n");
+    printf("5. Quick sort\n");
+    printf("6. Merge sort\n");
+    printf("Enter 0 to close the programm.\n");
+    printf("Your choice: ");
+}
 void main()
 {
-    int a[N];
-    setlocale(LC_ALL, "Russian");
-    scan(a,N);
-    print(a,N);
-    choose(menu());
+    int i = 0;
+    int exit = 1;
+    double total_time;
+    char e;
+    int choice;
+    clock_t start, end;
+    wchar_t** fileNames;
+    wchar_t* ca;
+    int* filesindex;
+    char* a;
+    ULONGLONG* filesize;
+    ULONGLONG* filesizemain;
+    int* newindex;
+	fileNames = (wchar_t**)malloc(MAX_FILES * sizeof(wchar_t*));
+    filesindex = (int*)malloc((kolvo + 1) * sizeof(int));
+    newindex = (int*)malloc(kolvo * sizeof(int));
+    filesizemain = (ULONGLONG*)malloc(MAX_FILES * sizeof(ULONGLONG));
+    filesize = (ULONGLONG*)malloc(MAX_FILES * sizeof(ULONGLONG));
+    a = (char*)malloc(MAX_LEN);
+    ca = (wchar_t*)malloc(MAX_LEN); 
+    printf("File Manager v1.0 is opening...\n");
+    while (1)
+    {
+		printf("Enter the file path\n");         
+        fgets(a, MAX_LEN, stdin);
+        a[strlen(a) - 1] = '\0';
+        swprintf(ca, MAX_LEN, L"%hs", a);
+        if (ListDirectoryContents(ca, filesize, fileNames) == 1) continue;
+        print(filesize, fileNames, kolvo);
+        for (i = 0; i < kolvo; i++)
+        {
+            filesindex[i] = i;
+            newindex[i] = i;
+        }
+        menu();
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Selection sort.\n");
+            choose(filesize, kolvo, newindex);
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[i]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+        case 2:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Insertion sort.\n");
+            insert(filesize, newindex, kolvo);
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[i]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+        case 3:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Bubble sort.\n");
+            bubblesort(filesize, newindex, kolvo);
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[i]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+        case 4:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Counting sort.\n");
+            countingsort(filesize, kolvo, newindex);
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[i]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+        case 5:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Quick sort.\n");
+            newindex = quicksort(filesize, 0, kolvo - 1, kolvo, filesindex);
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[i]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+        case 6:
+            scanf("%c", &e);
+            zeroing(filesizemain, filesize, filesindex, newindex, kolvo);
+            start = clock();
+            printf("Merge sort.\n");
+            mergesort(newindex, filesize, 0, (kolvo - 1));
+            end = clock();
+            for (i = 0; i < kolvo; i++)
+            {
+                wprintf(L"file %s size %lld bytes\n", fileNames[newindex[i]], filesize[newindex[i]]);
+            }
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("Time: %.5lf sec\n", total_time);
+            break;
+		case 0:
+            free(filesize);
+            free(filesindex);
+            for (i = 0; i < kolvo; i++) free(fileNames[i]);
+            free(fileNames);
+            free(filesizemain);
+            free(newindex);
+            return;
+        default:
+            printf("An error occurred. Please try again:\n");
+            scanf("%d", &choice);
+        }
+        kolvo = 0;
+        printf("If you want to close the programm, enter 0, otherwise enter any number\n");
+        scanf("%d", exit);
+        if (exit == 0)
+        {
+            free(filesize);
+            free(filesindex);
+            for (i = 0; i < kolvo; i++) free(fileNames[i]);
+            free(fileNames);
+            free(filesizemain);
+            free(newindex);
+            return;
+        }
+    }
 }
