041a85ae356580c4a3c35f983b6fb1a87e2ca027
taktaev-artyom <43129320+taktaev-artyom@users.noreply.github.com>
2019-05-21 09:55:38 +0300

    delete maxsize vse OK

--- taktaev_aa/Practice/Practice10/Container.h
+++ taktaev_aa/Practice/Practice10/Container.h
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-template <typename T, int maxsize>
+template <typename T>
 class Container
 {
 	T* arr;
 	int realsize;
+	int maxsize;
 public:
 	Container();
 	Container(const Container& a);
@@ -17,18 +18,20 @@ public:
 	int Find(T a) const;
 	void Add(T a);
 	void Delete(T a);
+	void resize();
 	void print();
 };
 
-template<typename T, int maxsize>
-Container<T, maxsize>::Container()
+template<typename T>
+Container<T>::Container()
 {
 	realsize = 0;
+	maxsize = 10;
 	arr = new T[maxsize];
 }
 
-template<typename T, int maxsize>
-Container<T, maxsize>::Container(const Container<T, maxsize>& a)
+template<typename T>
+Container<T>::Container(const Container<T>& a)
 {
 	arr = new T[maxsize];
 	realsize = a.realsize;
@@ -36,26 +39,26 @@ Container<T, maxsize>::Container(const Container<T, maxsize>& a)
 		arr[i] = a.arr[i];
 }
 
-template<typename T, int maxsize>
-Container<T, maxsize>::~Container()
+template<typename T>
+Container<T>::~Container()
 {
 	delete[] arr;
 }
 
-template<typename T, int maxsize>
-bool Container<T, maxsize>::IsFull() const
+template<typename T>
+bool Container<T>::IsFull() const
 {
 	return (realsize == maxsize);
 }
 
-template<typename T, int maxsize>
-bool Container<T, maxsize>::IsEmpty() const
+template<typename T>
+bool Container<T>::IsEmpty() const
 {
 	return (realsize == 0);
 }
 
-template<typename T, int maxsize>
-int Container<T, maxsize>::Find(T a) const
+template<typename T>
+int Container<T>::Find(T a) const
 {
 	if (IsEmpty()) throw "Container is empty(nothing to find)";
 	for (int i = 0; i < realsize; i++)
@@ -63,15 +66,15 @@ int Container<T, maxsize>::Find(T a) const
 	return -1;
 }
 
-template<typename T, int maxsize>
-void Container<T, maxsize>::Add(T a)
+template<typename T>
+void Container<T>::Add(T a)
 {
-	if (IsFull()) throw "Container is full(cannot add an element)";
+	if (IsFull()) resize();
 	arr[realsize++] = a;
 }
 
-template<typename T, int maxsize>
-void Container<T, maxsize>::Delete(T a)
+template<typename T>
+void Container<T>::Delete(T a)
 {
 	if (IsEmpty()) throw "Container is empty(nothing to delete)";
 	int idx = Find(a);
@@ -79,8 +82,22 @@ void Container<T, maxsize>::Delete(T a)
 	arr[idx] = arr[--realsize];
 }
 
-template<typename T, int maxsize>
-void Container<T, maxsize>::print()
+template <typename T>
+void Container<T>::resize()
+{
+	T* tmparr = new T[realsize];
+	for (int i = 0; i < realsize; i++)
+		tmparr[i] = arr[i];
+	delete[] arr;
+	maxsize += 5;
+	arr = new T[maxsize];
+	for (int i = 0; i < realsize; i++)
+		arr[i] = tmparr[i];
+	delete[] tmparr;
+}
+
+template<typename T>
+void Container<T>::print()
 {
 	if (IsEmpty()) throw "Container is empty(nothing to print)";
 	for (int i = 0; i < realsize; i++)
@@ -89,14 +106,15 @@ void Container<T, maxsize>::print()
 
 ///////////////////////////////////////////////////////////////////////////////
 
-template <typename T, int maxsize>
-class Container<T*, maxsize>
+template <typename T>
+class Container<T*>
 {
 	T** arr;
 	int realsize;
+	int maxsize;
 public:
 	Container();
-	Container(const Container<T*, maxsize>& a);
+	Container(const Container<T*>& a);
 	~Container();
 	bool IsFull() const;
 	bool IsEmpty() const;
@@ -106,15 +124,16 @@ public:
 	void print();
 };
 
-template<typename T, int maxsize>
-Container<T*, maxsize>::Container()
+template<typename T>
+Container<T*>::Container()
 {
 	realsize = 0;
+	maxsize = 10;
 	arr = new T*[maxsize];
 }
 
-template<typename T, int maxsize>
-Container<T*, maxsize>::Container(const Container<T*, maxsize>& a)
+template<typename T>
+Container<T*>::Container(const Container<T*>& a)
 {
 	arr = new T*[maxsize];
 	realsize = a.realsize;
@@ -122,27 +141,27 @@ Container<T*, maxsize>::Container(const Container<T*, maxsize>& a)
 		arr[i] = new T(*(a.arr[i]));
 }
 
-template<typename T, int maxsize>
-Container<T*, maxsize>::~Container()
+template<typename T>
+Container<T*>::~Container()
 {
 	for (int i = 0; i < realsize; i++)
 		delete arr[i];
 }
 
-template<typename T, int maxsize>
-bool Container<T*, maxsize>::IsFull() const
+template<typename T>
+bool Container<T*>::IsFull() const
 {
 	return (realsize == maxsize);
 }
 
-template<typename T, int maxsize>
-bool Container<T*, maxsize>::IsEmpty() const
+template<typename T>
+bool Container<T*>::IsEmpty() const
 {
 	return (realsize == 0);
 }
 
-template<typename T, int maxsize>
-int Container<T*, maxsize>::Find(T a) const
+template<typename T>
+int Container<T*>::Find(T a) const
 {
 	if (IsEmpty()) throw "Container is empty(nothing to find)*";
 	for (int i = 0; i < realsize; i++)
@@ -150,15 +169,15 @@ int Container<T*, maxsize>::Find(T a) const
 	return -1;
 }
 
-template<typename T, int maxsize>
-void Container<T*, maxsize>::Add(T a)
+template<typename T>
+void Container<T*>::Add(T a)
 {
-	if (IsFull()) throw "Container is full(cannot add an element)*";
+	if (IsFull()) maxsize++;
 	arr[realsize++] = new T(a);
 }
 
-template<typename T, int maxsize>
-void Container<T*, maxsize>::Delete(T a)
+template<typename T>
+void Container<T*>::Delete(T a)
 {
 	if (IsEmpty()) throw "Container is empty(nothing to delete)*";
 	int idx = Find(a);
@@ -167,8 +186,8 @@ void Container<T*, maxsize>::Delete(T a)
 	arr[idx] = arr[--realsize];
 }
 
-template<typename T, int maxsize>
-void Container<T*, maxsize>::print()
+template<typename T>
+void Container<T*>::print()
 {
 	if (IsEmpty()) throw "Container is empty(nothing to print)*";
 	for (int i = 0; i < realsize; i++)
